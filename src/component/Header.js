c229800ed58606dac560da6a7bb6515fb395a14c
priyankaahire <psahire01@gmail.com>
2023-06-13 10:33:39 +0530

    login button css

--- src/component/Header.js
+++ src/component/Header.js
@@ -6,13 +6,20 @@ const Title = () => (
       <h2 >EatSome24</h2>
     </a>
 )
+const loggedInUser = () => {
+  //API call to check autehticatio
 
+  return true;
+}
 const HeaderComponent = ({handleClick}) => {
   const [searchInputText, setSearchInputtext] = useState() //# TO CREATE THE STATE VARIBALE WITH DEFAULT VALUE 
   const handleChange = e => {
     setSearchInputtext(e.target.value);
     handleClick(e);
   };
+
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+ 
  
     return (
         <header>
@@ -30,8 +37,23 @@ const HeaderComponent = ({handleClick}) => {
                         <li>About Us</li>
                         <li>Contact</li>
                         <li>Cart</li>
+                        <li className='login-btn'>
+                        {
+                  //* Can not write statemnet like a = 10; console.log(a)
+                  //* You can write expression console.log() or ((a=10), conwol3.log(a))
+                  isLoggedIn ? (
+                  <button onClick={()=>setIsLoggedIn(false)}>Logout</button>
+                  ) :  
+                  (
+                  <button onClick={() => setIsLoggedIn(true)}>Login</button>
+                  )
+                }
+                        </li>
                     </ul>
                 </div>
+              
+               
+
              </div>
             
             </div>
